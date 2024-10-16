d2b7d09a39d52b95919c34b2a5ac93a072d685ee
John Doe <mahdibenalaya566@gmail.com>
2024-10-16 19:52:35 +0100

    prod

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import Navbar from "@/components/Navbar/Navbar";
 export const dynamic = 'force-dynamic';
 export default async function Home() {
    try {
-    var  res = await axios.get(`${process.env.Base_url}/api/movie/`)
+    var  res = await axios.get(`/api/movie/`)
    } catch (error) {
     return Promise.reject(error);
    }
